d46d0f067083cd48e97a6b428d2a7a86cbe7dcfb
Bryce Borba <borbaesb@gmail.com>
2022-03-03 11:48:28 -0800

    added prompts to add new teammember, phase 1 completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace planYourHeist
+namespace PlanYourHeist
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine("What is your name, Comrade Member?");
+            TeamMember juryB = new TeamMember();
+            juryB.Name = Console.ReadLine();
+            Console.WriteLine("What is your skill level?");
+            juryB.SkillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your courage level?");
+            juryB.CourageFactor = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Comrade {juryB.Name} has a skill level of {juryB.SkillLevel} and a courage factor of {juryB.CourageFactor}");
         }
     }
 }
